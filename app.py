703814da9796e3515f449ed35143a553d857d078
Twilight <1766281103@qq.com>
2024-03-13 16:52:53 +0800

    预览界面

--- app.py
+++ app.py
@@ -34,24 +34,25 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 @app.route('/data', methods=['POST'])
-def handle_data():
+def handle_data():#修改成三个数据
     try:
         # if 'files' in request.files:
             # 获取上传的文件数据
         print('loading')
         file = request.files['files']
         # 处理文件数据，例如保存到本地或者进行其他操作
+
         filename = file.filename
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-
         # 读取上传的文件内容并转换为模型输入格式（假设为CSV文件）
         df = pd.read_csv(file_path, encoding='gbk')
         #特征处理
-        df['max月统筹金占总比例'] = df['月统筹金额_MAX'] /df['统筹支付金额_SUM']
+        df['月就诊天数_SUM'] = df['月就诊天数_AVG'] * df['就诊的月数']
+        df['max占最大月的百分比'] = df['月统筹金额_MAX'] /df['统筹支付金额_SUM']
         df = df.fillna(0)
         # 提取特征列
-        X = df[['max月统筹金占总比例', '月统筹金额_MAX', '月就诊次数_MAX', '本次审批金额_SUM', '月药品金额_AVG']]
+        X = df[['月统筹金额_MAX','月就诊天数_SUM','月就诊次数_MAX','统筹支付金额_SUM','本次审批金额_SUM','max占最大月的百分比']]
 
         # 进行预测
         predictions = model.predict(X)
