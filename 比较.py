3da0d75b1f1ca5b336523715d65f0b8ffa5b5a1b
Twilight <1766281103@qq.com>
2024-05-20 20:10:28 +0800

    1.1.0
    添加了一个比较的文件

---
+++ 比较.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# 读取 CSV 文件
+pred_df = pd.read_csv('uploads/test - 2000.csv')
+actual_df = pd.read_csv(r'C:\Users\17662\Desktop\校内\大二下\1-第十五届服创大赛参赛手册、赛题手册等系列材料\训练数据\test - 2000.csv')
+
+# 提取结果列
+y_pred = pred_df['RES']
+y_true = actual_df['RES']
+
+# 确保两个文件的长度相同
+assert len(y_pred) == len(y_true), "两个文件的行数不一致，无法比较。"
+
+# 计算一致率
+accuracy = (y_pred == y_true).mean()
+
+print(f'一致率: {accuracy * 100:.2f}%')
